ad19f14f65e9d5a5913d7bfe05051bde060c308e
Lucas Emanuel <lukasgomes10@hotmail.com>
2025-07-07 15:53:56 -0300

    feat: criando service de consumo graphl com apolloAngular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AnimeListService } from './services/anime-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true,
+  imports: [CommonModule],
+  template: `<h1>Testando Apollo</h1>`,
 })
 export class App {
-  protected title = 'anime-list';
+  constructor(private animeService: AnimeListService) {}
+
+  // ngOnInit() {
+  //   this.animeService.searchAnime('Naruto').subscribe({
+  //     next: (result) => {
+  //       console.log('Resultado Apollo:', result.data?.Page?.media);
+  //     },
+  //     error: (err) => {
+  //       console.error('Erro na consulta Apollo:', err);
+  //     },
+  //   });
+  // }
 }
--- src/app/services/anime-list.ts
+++ src/app/services/anime-list.ts
@@ -2,12 +2,26 @@ import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { gql } from 'graphql-tag';
 
+interface SearchAnimeResponse {
+  Page: {
+    media: {
+      id: number;
+      title: {
+        romaji: string;
+      };
+      coverImage: {
+        large: string;
+      };
+    }[];
+  };
+}
+
 @Injectable({ providedIn: 'root' })
 export class AnimeListService {
   constructor(private apollo: Apollo) {}
 
   searchAnime(title: string) {
-    return this.apollo.query({
+    return this.apollo.query<SearchAnimeResponse>({
       query: gql`
         query ($search: String) {
           Page(page: 1, perPage: 10) {
