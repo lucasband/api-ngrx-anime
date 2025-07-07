315b4548f26c26a6b0a2b5387815abf773b0b018
Lucas Emanuel <lukasgomes10@hotmail.com>
2025-07-07 16:46:54 -0300

    feat: Integrando telas iniciais para consumo de api

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AnimeListService } from './services/anime-list';
+import { AnimeCardComponent } from './shared/anime-card/anime-card';
+import { Navbar } from "./shared/navbar/navbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  template: `<h1>Testando Apollo</h1>`,
+  imports: [CommonModule, FormsModule, AnimeCardComponent, Navbar],
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss']
 })
 export class App {
+  searchTerm = 'HunterxHunter'
+  animes: any[] = [];
+  loading = false;
+
   constructor(private animeService: AnimeListService) {}
 
-  //Teste de consumo de api 
+  buscar() {
+    this.loading = true;
+    this.animeService.searchAnime(this.searchTerm).subscribe({
+      next: (res) => {
+        this.animes = res.data.Page.media;
+        this.loading = false;
+      },
+      error: () => {
+        this.animes = [];
+        this.loading = false;
+      }
+    });
+  }
 
-  // ngOnInit() {
-  //   this.animeService.searchAnime('Naruto').subscribe({
-  //     next: (result) => {
-  //       console.log('Resultado Apollo:', result.data?.Page?.media);
-  //     },
-  //     error: (err) => {
-  //       console.error('Erro na consulta Apollo:', err);
-  //     },
-  //   });
-  // }
-  
+  ngOnInit() {
+    this.buscar();
+  }
 }
